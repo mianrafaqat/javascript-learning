f05c6bc230506213770a4e8878efd0d1504d166b
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2023-05-17 11:19:49 +0500

    basic methods

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-const arr = ["a", "b", "c", "d"];
-console.log(arr);
-console.log(arr.slice());
-console.log(arr.slice(2));
-console.log(arr.splice(1, 2));
+// const arr = ["a", "b", "c", "d"];
+// console.log(arr);
+// console.log(arr.slice());
+// console.log(arr.slice(2));
+// console.log(arr.splice(1, 2));
 
-console.log(arr.slice(1, 2));
-console.log(arr);
+// console.log(arr.slice(1, 2));
+// console.log(arr);
+
+// reverse
+
+// const arr2 = ["j", "i", "h", "g", "f", "e"];
+// console.log(arr2.reverse());
+
+//concat
+
+// const letters = arr.concat(arr2);
+// console.log(letters);
+
+// join
+
+// console.log(letters.join("-"));
+
+//at method
+const arr = [23, 11, 64];
+console.log(arr.at(0));
+// getting the last index value
+console.log(arr.at(-1));
+// getting the first index value
+console.log(arr.at(1));
