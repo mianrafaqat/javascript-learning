936a518551ff8021d2b34edda9e45c806ca26a1c
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2023-05-17 11:04:09 +0500

    arrays

---
+++ script.js
@@ -0,0 +1,8 @@
+const arr = ["a", "b", "c", "d"];
+console.log(arr);
+console.log(arr.slice());
+console.log(arr.slice(2));
+console.log(arr.splice(1, 2));
+
+console.log(arr.slice(1, 2));
+console.log(arr);
